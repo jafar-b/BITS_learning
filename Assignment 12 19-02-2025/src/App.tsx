97c2101fe9811f122a112871ab2d0227bf9a8bdb
jafar-b <beldarjafar@gmail.com>
2025-02-19 19:45:31 +0530

    PostsApp using useEffects,useState and axios

--- Assignment 12 19-02-2025/src/App.tsx
+++ Assignment 12 19-02-2025/src/App.tsx
@@ -1,11 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {
-  TextField,
-  Box,
-  CircularProgress,
-} from "@mui/material";
-
+import { TextField, Box, CircularProgress, Button } from "@mui/material";
 import "./App.css";
 
 type Post = {
@@ -19,7 +14,15 @@ function App() {
   const [id, setId] = useState<number | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [postError, setPostError] = useState<boolean>(false);
-  
+  const [newPostId, setNewPostId] = useState<number | null>(null); //used for setting the id received from axios post request  //used further to display the ID in the UI
+
+  //for new post
+  const [newPost, SetNewPost] = useState<Post>({
+    id: null,
+    userId: null,
+    title: "",
+    body: "",
+  });
 
   useEffect(() => {
     if (id !== null) {
@@ -49,69 +52,164 @@ function App() {
     }
   };
 
+  const handleNewPost = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `https://jsonplaceholder.typicode.com/posts`,
+        JSON.stringify(newPost),
+        { headers: { "Content-Type": "application/json" } }
+      );
+      setNewPostId(await res.data.id);
+      console.log(await res.data);
+    } catch (err) {
+      console.log("error in post request", err);
+    }
+  };
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    SetNewPost({ ...newPost, [e.target.name]: e.target.value });
+  };
 
   return (
     <>
-      <Box
-        display="flex"
-        flexDirection="column"
-        gap={2}
-        width={400}
-        marginBottom={2}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
       >
-        <TextField
-          label="Enter post ID"
-          variant="outlined"
-          value={id ?? null}
-          onChange={(e) => {
-            setId(Number(e.target.value));
-            setPostError(false);
-          }}
-          fullWidth
-        />
-       
-      </Box>
+        <h2>Fetch Post using ID</h2>
+        <div>
+          <Box
+            display="flex"
+            flexDirection="column"
+            gap={2}
+            width={400}
+            marginBottom={2}
+          >
+            <TextField
+              label="Fetch post using ID"
+              variant="outlined"
+              value={id ?? ""}
+              onChange={(e) => {
+                setId(Number(e.target.value));
+                setPostError(false);
+              }}
+              fullWidth
+            />
+          </Box>
 
-      {!postError ? (
-        loading ? (
-          <CircularProgress />
-        ) : (
-          post && (
-            <>
-            <Box
-              display="flex"
-              flexDirection="column"
-              gap={1}
-              padding={2}
-              border={1}
-              alignItems={"flex-start"}
-              borderRadius={2}
-              width="90%"
-            >
-              <div>
-                <strong>Title:</strong> {post.title}
-              </div>
-              <div>
-                <strong>User ID:</strong> {post.userId}
-              </div>
-              <div>
-                <strong>Post ID:</strong> {post.id}
-              </div>
-              <div>
-                <strong>Body:</strong> {post.body}
-              </div>
-            </Box>
-            {/* <button style={{marginTop:"10px"}} onClick={handleEdit}>Edit Post</button> */}
-            </>
-          )
-        )
-      ) : (
-        <h1>Post not found</h1>
-      )}
+          {!postError ? (
+            loading ? (
+              <CircularProgress />
+            ) : (
+              post && (
+                <>
+                  <Box
+                    display="flex"
+                    flexDirection="column"
+                    gap={1}
+                    padding={2}
+                    border={1}
+                    alignItems={"flex-start"}
+                    borderRadius={2}
+                    width="90%"
+                  >
+                    <div>
+                      <strong>Title:</strong> {post.title}
+                    </div>
+                    <div>
+                      <strong>User ID:</strong> {post.userId}
+                    </div>
+                    <div>
+                      <strong>Post ID:</strong> {post.id}
+                    </div>
+                    <div>
+                      <strong>Body:</strong> {post.body}
+                    </div>
+                  </Box>
+                </>
+              )
+            )
+          ) : (
+            <h1>Post not found</h1>
+          )}
+        </div>
+        <div>
+          <Box
+            display="flex"
+            flexDirection="column"
+            marginTop={5}
+            gap={2}
+            width={400}
+            component="form"
+            onSubmit={handleNewPost}
+          >
+            <h2>Add a new Post</h2>
+            <TextField
+              label="User ID"
+              variant="outlined"
+              name="userId"
+              value={newPost?.userId || ""}
+              onChange={handleChange}
+              fullWidth
+            />
 
+            <TextField
+              label="Title"
+              variant="outlined"
+              name="title"
+              value={newPost?.title || ""}
+              onChange={handleChange}
+              fullWidth
+            />
+            <TextField
+              label="Body"
+              variant="outlined"
+              name="body"
+              value={newPost?.body || ""}
+              onChange={handleChange}
+              fullWidth
+              multiline
+              rows={3}
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Add Post
+            </Button>
 
-
+            {newPostId && ( //displaying component only when newPostId is recieved
+              <>
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  gap={1}
+                  padding={2}
+                  border={1}
+                  alignItems={"flex-start"}
+                  borderRadius={2}
+                  width="90%"
+                >
+                  <h3>Post Added</h3>
+                  <div>
+                    <strong>Title:</strong> {newPost.title}
+                  </div>
+                  <div>
+                    <strong>User ID:</strong> {newPost.userId}
+                  </div>
+                  <div>
+                    <strong>Post ID:</strong> {newPostId}
+                  </div>
+                  <div>
+                    <strong>Body:</strong> {newPost.body}
+                  </div>
+                </Box>
+              </>
+            )}
+          </Box>
+        </div>
+      </div>
     </>
   );
 }
